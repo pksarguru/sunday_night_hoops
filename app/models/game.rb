7b92555d6562fba43c15d9c351952e2ee45d492b
Pavan Sarguru <pksarguru@users.noreply.github.com>
2018-01-09 00:50:54 -0600

    wip

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,8 @@
 class Game < ApplicationRecord
   has_many :participants, :class_name => "Player", :dependent => :destroy
   has_many :people, :through => :participants, :source => :person
+
+  def game_on?
+    # method to count total players + guest is above 8
+  end
 end
